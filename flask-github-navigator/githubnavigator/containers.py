72269e118e5ac8e79ff2c15463facf74a4fe9476
Michael Haynes <Michael.Haynes@oati.net>
2023-10-23 14:40:00 -0500

    Implement SearchService

--- flask-github-navigator/githubnavigator/containers.py
+++ flask-github-navigator/githubnavigator/containers.py
@@ -1,6 +1,8 @@
 from dependency_injector import containers, providers
 from github import Github
 
+from . import services
+
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration(yaml_files=["config.yaml"])
@@ -9,4 +11,9 @@ class Container(containers.DeclarativeContainer):
         Github,
         login_or_token=config.github.auth_token,
         timeout=config.github.request_timeout,
+    )
+
+    search_service = providers.Factory(
+        services.SearchService,
+        github_client=github_client,
     )
