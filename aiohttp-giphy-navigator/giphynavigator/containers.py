8aba3d0b2d795c3562c39feadc3215d17bdc79d4
Michael Haynes <Michael.Haynes@oati.net>
2023-10-23 11:48:54 -0500

    Implement search service

--- aiohttp-giphy-navigator/giphynavigator/containers.py
+++ aiohttp-giphy-navigator/giphynavigator/containers.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
 
-from . import giphy
+from . import giphy, services
 
 
 class Container(containers.DeclarativeContainer):
@@ -10,4 +10,15 @@ class Container(containers.DeclarativeContainer):
         giphy.GiphyClient,
         api_key=config.giphy.api_key,
         timeout=config.giphy.request_timeout,
+    )
+
+    giphy_client = providers.Factory(
+        giphy.GiphyClient,
+        api_key=config.giphy.api_key,
+        timeout=config.giphy.request_timeout,
+    )
+
+    search_service = providers.Factory(
+        services.SearchService,
+        giphy_client=giphy_client,
     )
