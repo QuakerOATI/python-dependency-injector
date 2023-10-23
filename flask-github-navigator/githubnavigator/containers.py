c3b7cbb5e894138073fb63d49e4550918a039b56
Michael Haynes <Michael.Haynes@oati.net>
2023-10-23 14:33:22 -0500

    Add GitHub API integration

--- flask-github-navigator/githubnavigator/containers.py
+++ flask-github-navigator/githubnavigator/containers.py
@@ -1,5 +1,12 @@
-from dependency_injector import containers
+from dependency_injector import containers, providers
+from github import Github
 
 
 class Container(containers.DeclarativeContainer):
-    ...
+    config = providers.Configuration(yaml_files=["config.yaml"])
+
+    github_client = providers.Factory(
+        Github,
+        login_or_token=config.github.auth_token,
+        timeout=config.github.request_timeout,
+    )
