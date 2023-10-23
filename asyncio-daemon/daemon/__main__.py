36d31ee201175a47bef2847524dfab2e628de6d4
Michael Haynes <Michael.Haynes@oati.net>
2023-10-23 10:25:51 -0500

    Add wiring in entrypoint
    Tested podman-compose up

--- asyncio-daemon/daemon/__main__.py
+++ asyncio-daemon/daemon/__main__.py
@@ -1,11 +1,17 @@
+from dependency_injector.wiring import Provide, inject
+
 from .containers import Container
+from .dispatcher import Dispatcher
 
 
-def main() -> None:
-    ...
+@inject
+def main(dispatcher: Dispatcher = Provide[Container.dispatcher]) -> None:
+    dispatcher.run()
 
 
 if __name__ == "__main__":
     container = Container()
     container.init_resources()
+    container.wire(modules=[__name__])
+
     main()
